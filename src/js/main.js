731f315a7c7c292c80fa428d6c7dd1f4826bffe0
joeappleton18 <joe.appleton@solent.ac.uk>
2018-02-01 13:55:18 +0000

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -11,11 +11,6 @@ import Vue from 'vue';
 import helloComponent from './components/hello.vue';
 Vue.component('hello-component', helloComponent);
 
-let im_cool = () => 'this is good';
-
-console.log(im_cool())
-
-
 const app = new Vue({
     el: '#app',
 });
